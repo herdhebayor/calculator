787bb671ef3bc17769e5a6e85bb060a22633108f
herdhebayor <98285362+herdhebayor@users.noreply.github.com>
2022-08-26 08:34:58 -0700

    added toggle switch for dark mode option

--- calc.js
+++ calc.js
@@ -8,7 +8,6 @@ function compute(){
     let y = eval(x)
     document.getElementById("input").innerHTML = y;       
    var history = document.getElementById("history").innerHTML += x + "</br>" + "= " + y  + "<br/>";
-   
 }
  function clrall(){
     document.getElementById("input").innerHTML=" "
@@ -18,4 +17,14 @@ function clr(){
     num = document.getElementById("input").innerHTML ;
     document.getElementById("input").innerHTML = num.substr(0,num.length -1);
  }
-
+ 
+//  DARK MODE TOGGLE
+ const myMove = ()=>{
+    console.log("my name")
+    const play = document.querySelector(".darkOrLight")
+    const main = document.getElementById("main")
+        play.classList.toggle("move");
+        main.classList.toggle("dark");
+       document.getElementById("input").style.color = "#fff"
+    
+}
